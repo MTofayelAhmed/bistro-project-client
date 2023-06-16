5cb706e970a6f7ba91650c6bd2acfb4538cfe708
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-06-17 01:16:18 +0600

    done with menuItem

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import Introduction from "../Introduction/Introduction";
+import PopularMenu from "../PopularMenu/PopularMenu";
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
     <Banner></Banner>
     <Category></Category>
     <Introduction></Introduction>
+    <PopularMenu></PopularMenu>
     </div>
   );
 };
---
+++ src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,18 @@
+
+
+const MenuItem = ({item}) => {
+  console.log("item", item)
+  const {name, price , recipe, image } = item;
+  return (
+    <div className="flex space-x-4">
+      <img style={{borderRadius: '0 200px 200px 200px'}} className="w-[118px], h-[104px] " src={image} alt="" />
+      <div className=" text-gray-500" >
+        <h2 className="uppercase">{name}-----------</h2>
+        <p>{recipe}</p>
+      </div>
+      <p className="text-yellow-500">${price}</p>
+    </div>
+  );
+};
+
+export default MenuItem;
