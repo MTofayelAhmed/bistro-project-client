f074cffcc16f4ab8d540b62c4b373572d941039d
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-06-22 22:28:33 +0600

    done with axios secure

---
+++ src/Hooks/useAxiosSecure.jsx
@@ -0,0 +1,47 @@
+
+import { useContext, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../Provider/AuthProvider/AuthProvider';
+
+
+
+
+const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const {logOut } = useContext(AuthContext)
+
+  const axiosSecure = axios.create({
+    baseURL: 'http://localhost:5000', 
+  });
+
+
+  useEffect(() => {
+    axiosSecure .interceptors.request.use((config) => {
+      const token = localStorage.getItem('access-token');
+      config.headers.Authorization = `Bearer ${token}`;
+      return config;
+    });
+
+    axiosSecure .interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response && (error.response.status === 401 || error.response.status === 403 )) {
+         
+            // Log out the user and navigate to the login page
+            logOut()
+            .then(() => {
+              navigate('/login');
+            });
+          
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, [ logOut, navigate, axiosSecure]);
+
+  return [axiosSecure];
+};
+
+export default useAxiosSecure;
+
--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -4,25 +4,32 @@ import {
 } from '@tanstack/react-query'
 import { useContext } from 'react';
 import { AuthContext } from '../Provider/AuthProvider/AuthProvider';
+import useAxiosSecure from './useAxiosSecure';
 
 
 
 const useCart = ()=> {
 const {user}= useContext(AuthContext)
-const token = localStorage.getItem('access-token')
+// const token = localStorage.getItem('access-token')
+const [axiosSecure] = useAxiosSecure()
 const {  data:cart=[], refetch } = useQuery({
   queryKey: ['carts', user?.email],
+//   queryFn:  async () => {
+//     const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`,{
+//       headers: {
+//         authorization: `bearer ${token}`
+//       }
+//     })
+//     return res.json()
+// }})
   queryFn:  async () => {
-    const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`,{
-      headers: {
-        authorization: `bearer ${token}`
-      }
-    })
-    return res.json()
+    const res = await axiosSecure(`/carts?email=${user?.email}`)
+    console.log('res from axios', res)
+    return res.data;
 }})
 return [cart, refetch]
 
 }
 
-export default useCart
+export default useCart;
 
