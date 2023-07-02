fa7de376c05b89672f3e8d611780ae1ac8553579
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-07-02 13:27:02 +0600

    done with charts

--- src/Pages/DashBoard/AdminHome/AdminHome.jsx
+++ src/Pages/DashBoard/AdminHome/AdminHome.jsx
@@ -2,65 +2,149 @@ import { useContext } from "react";
 import { AuthContext } from "../../../Provider/AuthProvider/AuthProvider";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
-
-
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Legend } from 'recharts';
+import { PieChart, Pie,ResponsiveContainer } from 'recharts';
 const AdminHome = () => {
-const {user}= useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
-const [axiosSecure] = useAxiosSecure()
+  const [axiosSecure] = useAxiosSecure()
+  const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+  Z`;
+  };
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
 
-const {data: stats={}}= useQuery({
-  queryKey: ['admin-stats'],
-  queryFn: async()=> {
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
 
-    const res = await axiosSecure('/admin-stats')
-    return res.data
-  }
+
+  const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+
+
+  const { data: stats = {} } = useQuery({
+    queryKey: ['admin-stats'],
+    queryFn: async () => {
+
+      const res = await axiosSecure('/admin-stats')
+      return res.data
+    }
+  })
+
+
+  
+  const { data: chartData = [] } = useQuery({
+    queryKey: ['chart-data'],
+    queryFn: async () => {
+        const res = await axiosSecure('/order-stats');
+        return res.data;
+    }
 })
   return (
     <div className="w-full m-4 ">
-      <h2 className=" text-3xl"> Hi, {user.displayName}</h2>
+      <h2 className=" text-3xl uppercase font-bold  ml-20 mb-10"> Hi, {user.displayName}</h2>
 
 
-      <div className="stats shadow flex gap-5">
-  
-  <div className="stat">
-    <div className="stat-figure text-primary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
-    </div>
-    <div className="stat-title">Revenue</div>
-    <div className="stat-value text-primary">{stats.revenue}</div>
-   
-  </div>
-  
-  <div className="stat">
-    <div className="stat-figure text-secondary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-    </div>
-    <div className="stat-title">Menu Item</div>
-    <div className="stat-value text-secondary">{stats.products}</div>
-   
-  </div>
-  <div className="stat">
-    <div className="stat-figure text-secondary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-    </div>
-    <div className="stat-title">Orders</div>
-    <div className="stat-value text-secondary">{stats.orders}</div>
-   
-  </div>
-  <div className="stat">
-    <div className="stat-figure text-secondary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-    </div>
-    <div className="stat-title">Users</div>
-    <div className="stat-value text-secondary">{stats.users}</div>
-   
-  </div>
-  
- 
-  
-</div>
+      <div className="stats shadow flex gap-5 mb-10 ml-5">
+
+        <div className="stat bg-pink-800 rounded-xl">
+          <div className="stat-figure text-primary">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+          </div>
+          <div className="stat-title text-white font-bold">Revenue</div>
+          <div className="stat-value text-primary">{stats.revenue}</div>
+
+        </div>
+
+        <div className="stat bg-sky-500 rounded-xl ">
+          <div className="stat-figure text-secondary">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+          </div>
+          <div className="stat-title text-white font-bold">Menu Item</div>
+          <div className="stat-value text-secondary">{stats.products}</div>
+
+        </div>
+        <div className="stat bg-gray-800 rounded-xl">
+          <div className="stat-figure text-secondary">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+          </div>
+          <div className="stat-title text-white font-bold">Orders</div>
+          <div className="stat-value text-secondary">{stats.orders}</div>
+
+        </div>
+        <div className="stat bg-purple-800 rounded-xl ">
+          <div className="stat-figure text-secondary">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+          </div>
+          <div className="stat-title font-bold text-white">Users</div>
+          <div className="stat-value text-secondary">{stats.users}</div>
+
+        </div>
+
+
+
+      </div>
+      <div className=" flex">
+        <div className=" w-1/2">
+          <BarChart
+            width={500}
+            height={300}
+            data={chartData}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="category" />
+            <YAxis />
+            <Bar dataKey="total" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+              {chartData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+              ))}
+            </Bar>
+          </BarChart>
+        </div>
+        <div className="w-1/2">
+                    <ResponsiveContainer width="100%" height="100%">
+                        <PieChart width={400} height={400}>
+                            <Legend></Legend>
+                            <Pie
+                                data={chartData}
+                                cx="50%"
+                                cy="50%"
+                                labelLine={false}
+                                label={renderCustomizedLabel}
+                                outerRadius={80}
+                                fill="#8884d8"
+                                dataKey="count"
+                            >
+                                {chartData.map((entry, index) => (
+                                    <Cell name={entry.category} key={`cell-${index}`} fill={COLORS[index % colors.length]} />
+                                ))}
+                            </Pie>
+                        </PieChart>
+                    </ResponsiveContainer>
+                </div>
+      </div>
     </div>
   );
 };
